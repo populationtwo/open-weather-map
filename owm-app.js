ef0c5c76e6b4e0253e0b7a21cb349c54b8859c6e
Edwin Lunandy <populationtwo@gmail.com>
2015-04-20 13:32:08 -0400

    Add router.

--- owm-app.js
+++ owm-app.js
@@ -1,1 +1,24 @@
-angular.module('OWMApp', ['ngRoute'])
+angular.module( 'OWMApp', ['ngRoute'] )
+	.value( 'owmCities', ['New York', 'Dallas', 'Chicago'] )
+	.config( ['$routeProvider', function ($routeProvider) {
+		$routeProvider.when( '/', {
+			templateUrl: 'home.html',
+			controller : 'HomeCtrl'
+		} ).when( '/cities/:city', {
+			templateUrl: 'city.html',
+			controller : 'CityCtrl'
+		} );
+	}] )
+	.controller( 'HomeCtrl', ['$scope', function ($scope) {
+
+	}] )
+	.controller( 'CityCtrl', function ($scope, $routeParams, owmCities) {
+		var city = $routeParams.city;
+		city = city.replace( '_', ' ' );
+		if (owmCities.indexOf( city ) == -1) {
+			//city not found
+			return;
+		}
+
+		$scope.city = city;
+	} );
